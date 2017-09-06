e32b7243e6780fcefc7474bd85a7647bbb3aa53b
Lloyd Grubham <grubham@gmail.com>
2017-09-06 16:07:45 -0600

    Stats currently displays a grid of statistics. Implementing graph display next.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      dice: 0
+      dice: 0,
+      stats: [],
+      summedStats: []
     }
     this.remOne = this.remOne.bind(this);
     this.addN = this.addN.bind(this);
@@ -16,15 +18,20 @@ class App extends Component {
   remOne(){
     let tempDice = this.state.dice - 1;
     this.setState({
-      dice: tempDice
+      dice: tempDice,
+      stats: oddsEach(tempDice),
+      summedStats: atLeast(tempDice)
     })
   }
   addN(n){
     let tempDice = this.state.dice + n;
     this.setState({
-      dice: tempDice
+      dice: tempDice,
+      stats: oddsEach(tempDice),
+      summedStats: atLeast(tempDice)
     })
   }
+ 
   
   render() {
     return (
@@ -33,10 +40,43 @@ class App extends Component {
           <h2>Liars Dice Stats! {this.state.dice}</h2>
         </div>
         <Inputs remOne={this.remOne} addN={this.addN}/>
-        <Stats/>
+        <Stats stats={this.state.stats} least={this.state.summedStats} />
       </div>
     );
   }
+}
+
+function factorial (n){
+  let out = 1;
+  for (let i = 2; i <= n ;i++) {
+    out *= i;
+  }
+  return n ? out: 1;
+}
+
+function odds(k, n){
+  let mult = factorial(n)/(factorial(k)*factorial(n-k));
+  let exp = Math.pow((1/3), k) * Math.pow((2/3),(n-k));
+  return mult * exp;
+}
+
+function oddsEach(n){
+  let out = [];
+  for (let i = 0; i <= n; i++){
+    out.push(Math.round(odds(i, n)*10000)/10000);
+  }
+  return out;
+}
+
+function atLeast(n){
+  let arr = [];
+  for (let i = 0; i <= n; i++){
+    arr.push(odds(i, n));
+  }
+  let out = arr.map((c,i,a)=>{
+    return Math.round(a.slice(i).reduce((s,v)=>{return s+v},0)*10000)/10000;
+  })
+  return out;
 }
 
 export default App;
--- src/Components/Stats.js
+++ src/Components/Stats.js
@@ -3,9 +3,18 @@ import React, { Component } from 'react';
 class Stats extends Component{
     
     render(){
+        const currentStats = this.props.stats.map((c,i,a)=>{
+            return(
+                <div className="statsItem" key={i}>
+                    <h5>Odds of rolling {i} of a kind: {Math.round(c * 10000)/100}%</h5>
+                    <hr/>
+                    <h5>Odds of rolling at least {i} of a kind: {Math.round(this.props.least[i] * 10000)/100}%</h5>
+                </div>
+            )
+        })
         return(
-            <div>
-                Stats Component
+            <div className="stats-container">
+                {currentStats}
             </div>
         )
     }
